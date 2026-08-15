7bd5ab3c8af45400523a197895c753eeb8e11b07
ajaichintala-hue <ajaichintala@gmail.com>
2026-08-15 20:04:28 +0530

    anagram checking function using two pointers
    Added a comment to describe the anagram checking function.

---
+++ anagramCheck_using_2pointers.py
@@ -0,0 +1,14 @@
+#anagram checking function using two pointers
+def isAnagram(s,t):
+    if len(s)!=len(t):
+       return False
+    countS={}
+    countT={}
+    for i in range(len(s)):
+        countS[s[i]]=countS.get(s[i],0)+1
+        countT[t[i]]=countT.get(t[i],0)+1
+    for c in countS:
+        if countS[c]!=countT.get(c,0):
+            return False
+    return True
+isAnagram()
